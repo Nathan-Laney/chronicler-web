66b8e0223963d95fb6ee6e3a69cbe0f5a962e041
Nathan-Laney <nathaniel.laney@gmail.com>
2025-08-15 12:15:48 -0400

    repair profile index

--- models/Profile.js
+++ models/Profile.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const profileSchema = new mongoose.Schema({
-  userId: { type: String, required: true, unique: true },
+  userId: { type: String, required: true },
   guildId: { type: String, required: true },
   experience: { type: Number, default: 0 },
   missions: { type: Array, default: [] },
 });
+
+profileSchema.index({ guildId: 1, userId: 1}, {unique: true});
 
 module.exports = mongoose.model("profiles", profileSchema);
